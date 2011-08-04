3fdb6782418af2e3feae6657c059470e64d32551
javajigi <javajigi@gmail.com>
2011-08-04 18:27:14 +0900

    [#13] end-to-end 테스트 기반을 마련한다. 단위 테스트와 통합 테스트를 같이 실행할 경우 깨지는 문제 해결함. 단 functionalTest도 unitTest와 같은 방식으로 구현할 경우 에러가 발생한다.

--- test/functional/AbstractFunctionalTest.java
+++ test/functional/AbstractFunctionalTest.java
@@ -2,16 +2,21 @@ package functional;
 
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Test;
 
-import play.mvc.Http.Response;
 import play.test.FunctionalTest;
 
 import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
 import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
 
 public abstract class AbstractFunctionalTest extends FunctionalTest {
-	protected final LocalServiceTestHelper helper = new LocalServiceTestHelper(new LocalDatastoreServiceTestConfig());
+	protected final LocalServiceTestHelper helper = createLocalServiceTestHelper();
+
+	private LocalServiceTestHelper createLocalServiceTestHelper() {
+		LocalDatastoreServiceTestConfig testConfig = new LocalDatastoreServiceTestConfig();
+		testConfig.setBackingStoreLocation("tmp/functionaltest-datastore");
+		LocalServiceTestHelper testHelper = new LocalServiceTestHelper(testConfig);
+		return testHelper;
+	}
 	
 	@Before
 	public void setup() {
--- test/models/AbstractDomainTest.java
+++ test/models/AbstractDomainTest.java
@@ -3,15 +3,26 @@ package models;
 import org.junit.After;
 import org.junit.Before;
 
+import play.Play;
 import play.test.UnitTest;
 
+import com.google.appengine.api.datastore.dev.LocalDatastoreService;
+import com.google.appengine.tools.development.ApiProxyLocal;
 import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
 import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
+import com.google.apphosting.api.ApiProxy;
 
 public abstract class AbstractDomainTest extends UnitTest {
 	static final long now = System.currentTimeMillis();
 	
-	protected final LocalServiceTestHelper helper = new LocalServiceTestHelper(new LocalDatastoreServiceTestConfig());
+	protected final LocalServiceTestHelper helper = createLocalServiceTestHelper();
+
+	private LocalServiceTestHelper createLocalServiceTestHelper() {
+		LocalDatastoreServiceTestConfig testConfig = new LocalDatastoreServiceTestConfig();
+		testConfig.setBackingStoreLocation("tmp/unittest-datastore");
+		LocalServiceTestHelper testHelper = new LocalServiceTestHelper(testConfig);
+		return testHelper;
+	}
 	
 	@Before
 	public void setup() {
